00ebfe3d0a819aa615117a88a129a5c9b96f7af3
arcticdiv <arcticdiv@users.noreply.github.com>
2021-04-12 19:26:49 +0200

    Add option to load keys from keys.ini, remove IDBE keys
    (the IDBE keys are obviously still in the git history, but I can't be bothered to rebase now)

--- nus_tools/config.py
+++ nus_tools/config.py
@@ -1,24 +1,63 @@
-import base64
-from dataclasses import dataclass
-from typing import Tuple
+import hashlib
+from configparser import ConfigParser
+from typing import Any, Optional
 from reqcli.config import Configuration as ReqCliConfig
 
 
-def _key_dec(val: str) -> bytes:  # tiny amount of obfuscation, seems better this way
-    return base64.b64decode(val)
+class IniKey:
+    def __init__(self, section, name, sha1):
+        self.section = section
+        self.name = name
+        self.sha1 = bytes.fromhex(sha1)
+        self.__value = None  # type: Optional[bytes]
+
+    def __get__(self, obj: Any, objtype: Any = None) -> bytes:
+        if self.__value is None:
+            raise RuntimeError(f'missing key: {self._var_name!r} (ini: {self.section}.{self.name})')
+        return self.__value
+
+    def __set__(self, obj: Any, value: bytes) -> None:
+        if hashlib.sha1(value).digest() != self.sha1:
+            raise RuntimeError(f'unexpected hash for key {self._var_name!r} (ini: {self.section}.{self.name})')
+        self.__value = value
+
+    def __set_name__(self, obj: Any, name: str) -> None:
+        self._var_name = name
 
 
-@dataclass
 class _Keys:
+    common_wiiu = IniKey('common', 'common_key_wiiu', '6a0b87fc98b306ae3366f0e0a88d0b06a2813313')
     # nn_idbe.rpl: .rodata+0x4c
-    idbe_iv: bytes = _key_dec('pGmHrkfYK7T6irwEUChfpA==')
-    idbe_keys: Tuple[bytes, ...] = tuple(
-        _key_dec(k) for k in
-        ['SrmkDhRpdahLsbTz7O/Eew==', 'kKC7Hg6GSuh9E6agPSjJuA==', '/7tXwU6Y7Gl1s4T89AeGtQ==', 'gJI3mbQfNqanX7i0jJX2bw==']
-    )
+    idbe_iv = IniKey('idbe', 'iv', '3db78243a8c9a89b399cc8e7511f06cbde9fa10b')
+    idbe_keys = [
+        IniKey('idbe', 'key0', '1ec1f7927524e8027985a1a3b2345b4d06c92152'),
+        IniKey('idbe', 'key1', '0fae601895044799eaaf5ce91f0f00509073824b'),
+        IniKey('idbe', 'key2', 'db73e30940500dcefdc3efe1b880af131fb7b745'),
+        IniKey('idbe', 'key3', '0bd3a8b30b8416afecd58dce4669c0e3e82a4ee7')
+    ]
+
+    def __init__(self):
+        p = ConfigParser()
+        if not p.read('keys.ini'):
+            return
+
+        for name, key in list(type(self).__dict__.items()):
+            # try to get value from ini
+            if isinstance(key, IniKey):
+                # single key
+                ini_value = p.get(key.section, key.name, fallback=None)
+                if ini_value is not None:
+                    setattr(self, name, bytes.fromhex(ini_value))
+            elif isinstance(key, list):
+                # possible list of keys
+                for i, el in enumerate(key):
+                    if not isinstance(el, IniKey):
+                        continue
+                    ini_value = p.get(el.section, el.name, fallback=None)
+                    if ini_value is not None:
+                        key[i] = bytes.fromhex(ini_value)
 
 
-@dataclass
 class _Configuration:
     keys: _Keys = _Keys()
 
