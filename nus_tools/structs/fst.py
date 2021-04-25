c96cd407657dd7e7a57565ad92afe81b93bd9f89
arcticdiv <arcticdiv@users.noreply.github.com>
2021-04-25 15:15:54 +0200

    Align FST struct to 0x8000 bytes

--- nus_tools/structs/fst.py
+++ nus_tools/structs/fst.py
@@ -1,6 +1,6 @@
 from construct import \
     Struct, Array, Byte, Bytes, CString, Int16ub, Int24ub, Int32ub, \
-    Const, FlagsEnum, IfThenElse, Padding, Check, this
+    Const, FlagsEnum, IfThenElse, Padding, Check, Aligned, FocusedSeq, Terminated, this
 from constructutils import InliningStruct, Inline
 
 
@@ -25,21 +25,25 @@ _entry = InliningStruct(
     'secondary_index' / Int16ub
 )
 
-struct = Struct(
-    Const(b'FST\0'),
-    'offset_factor' / Int32ub,
-    'num_secondary' / Int32ub,
-    '_unk1' / Bytes(0x14),
-    'secondary' / Array(this.num_secondary, Struct(
-        'offset_sectors' / Int32ub,
-        'size_sectors' / Int32ub,
-        'title_id' / Bytes(8),
-        'group_id' / Bytes(4),
-        'flags' / FlagsEnum(Int16ub, hash_tmd=0x100, hash_tree=0x200),
-        Padding(0x0a)
+struct = FocusedSeq(
+    'fst',
+    'fst' / Aligned(0x8000, Struct(
+        Const(b'FST\0'),
+        'offset_factor' / Int32ub,
+        'num_secondary' / Int32ub,
+        '_unk1' / Bytes(0x14),
+        'secondary' / Array(this.num_secondary, Struct(
+            'offset_sectors' / Int32ub,
+            'size_sectors' / Int32ub,
+            'title_id' / Bytes(8),
+            'group_id' / Bytes(4),
+            'flags' / FlagsEnum(Int16ub, hash_tmd=0x100, hash_tree=0x200),
+            Padding(0x0a)
+        )),
+        'root' / _entry,
+        Check(this.root.type.directory),
+        'entries' / Array(this.root.next_entry_index - 1, _entry),
+        'names' / Array(this.root.next_entry_index, CString('ascii'))
     )),
-    'root' / _entry,
-    Check(this.root.type.directory),
-    'entries' / Array(this.root.next_entry_index - 1, _entry),
-    'names' / Array(this.root.next_entry_index, CString('ascii'))
+    Terminated
 )
